5aa251d7b44b0bcb142f396906d9fcd49507ae48
HJ1510 <yhj221004@gmail.com>
2022-12-02 17:57:09 +0900

    jblog mysql

--- docs/jblog sql.sql
+++ docs/jblog sql.sql
@@ -1,13 +1,20 @@
 show tables;
 desc user;
 select * from `user` u ;
+delete from user;
 desc blog;
 select * from blog b ;
+delete from blog ;
 desc category;
 select * from category c ;
+delete from category ;
 desc post;
 select * from post p ;
 
 insert into user values(id, name, password);
 insert into blog values(id, title, profile);
 insert into category values(null, title, id);
+
+-- user login
+select name from user where id='ddo@gmail.com' and password='1234';
+

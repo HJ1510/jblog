bbe7f2c409ec0f3a10bcc6be2d18c08e7139ff3c
HJ1510 <yhj221004@gmail.com>
2022-12-14 17:51:25 +0900

    jblog sql

--- docs/jblog sql.sql
+++ docs/jblog sql.sql
@@ -45,31 +45,34 @@ UPDATE blog set title='고길동의 블로그', profile='profile' where id='go';
 insert into category values(null, '기본', '카테고리를 지정하지 않은 경우', 'go');
 insert into category values(null, '일기', '하루 기록', 'go');
 
-SELECT c.`no`, c.title , c.`desc`, (SELECT count(p.no) FROM post p where category_no = 6) as countPost -- countPost 구현..ㅜㅜ
-from category c , post p, blog b 
-where b.id = c.id and c.no = p.category_no and b.id ='go' order by c.no desc;
+SELECT c.no, c.title, c.desc from category c, blog b where b.id = c.id and b.id = 'go' order by c.no desc
 
-SELECT c.no, c.title, c.desc from category c, blog b where b.id = c.id and b.id = 'go' order by c.no desc;
+-- 카테고리별 글 갯수
+SELECT c.no, c.title , c.`desc`, category_no , count(*) as countPost from post p , category c where p.category_no = c.`no` and id ='go' group by category_no order by c.no desc; 
 
-(SELECT count(p.no) FROM post p where category_no = 6) as a;
-
-DELETE from category where no=11;
+SELECT c.no, c.title, c.desc , IFNULL(countPost, 0) as countPost
+from category c left outer join (SELECT p.category_no , count(p.no) as countPost from post p group by category_no) as p
+on c.no = p.category_no
+where id='do'
+group by c.no 
+order by c.no desc;
 
 -- admin write
 INSERT into post values(null, '두번째 글입니다', '스프링 공부진행중', now(), 6);
-INSERT into post values(null, '1208', '일기 씀', now(), 8);
+INSERT into post values(null, '오류1', '수정내역1', now(), 17);
+INSERT into post values(null, '1214', '일기 씀', now(), 8);
 INSERT into post values(null, '도우너 첫 글', '도우너예요 블로그 시작합니다', now(), 7);
 SELECT c.no as categoryNo, c.title as categoryTitle from category c, blog b where b.id = c.id and b.id = 'go' order by c.no asc;
 SELECT p.category_no as categoryNo from post p;
 SELECT * from post p , category c where p.category_no = c.no;
 
 -- blog index
-SELECT b.title , b.profile , c.title as categoryTitle, p.title as postTitle, p.contents, p.reg_date as regDate from blog b , category c , post p where b.id =c.id and c.`no` =p.category_no ;
+SELECT b.title , b.profile , c.title as categoryTitle, p.title as postTitle, p.contents, p.reg_date as regDate from blog b , category c , post p where b.id =c.id and b.id='ddo' and c.`no` =p.category_no ;
 
 SELECT title , contents, reg_date as regDate from post p where max(regDate);
 
--- 가장 최근 글
-SELECT c.title as categoryTitle, p.title as postTitle , p.contents FROM post p, category c where p.category_no =c.`no` and id ='go' ORDER BY reg_date DESC LIMIT 1;
+-- 카테고리별 최근 글
+SELECT c.title as categoryTitle, p.title as postTitle , p.contents FROM post p, category c where p.category_no =c.`no` and id ='go' and c.`no` = 17 ORDER BY reg_date DESC LIMIT 1;
 
--- 카테고리 리스트(최근글 날짜 포함)
-SELECT c.title as categoryTitle, p.reg_date as regDate() FROM post p, category c where p.category_no =c.`no` and id='go' ORDER BY reg_date desc;
+-- 글 목록
+SELECT p.title, reg_date as regDate FROM post p, category c where p.category_no =c.`no` and c.id='go' order by p.no DESC ;
